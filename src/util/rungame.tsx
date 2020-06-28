a541457ad32e7212b8cc914cbad754572fbbb451
rajderks <2419711+rajderks@users.noreply.github.com>
2020-06-28 15:32:59 +0200

    v5.0.1

--- src/util/rungame.tsx
+++ src/util/rungame.tsx
@@ -1,34 +1,19 @@
 import fs from 'fs';
-import { execFile, spawn, exec } from 'child_process';
+import { exec } from 'child_process';
 import { BASE_URI } from '../constants';
 import { logEntry } from './logger';
-
-const callback = (error: Error | null, stdout: string, stderr: string) => {
-  if (error) {
-    console.error(error);
-  }
-  if (stdout) {
-    console.error(stdout);
-  }
-  if (stderr) {
-    console.error(stderr);
-  }
-};
 
 const rungame = () => {
   const BASE_URI_WIN = BASE_URI.replace(/\//g, '\\');
   fs.stat(`${BASE_URI}/bin/ForgedAlliance.exe`, (errFA) => {
     if (errFA) {
-      // try SupCom
       fs.stat(`${BASE_URI}/bin/SupremeCommander.exe`, (errSC) => {
         if (errSC) {
           logEntry(
             `Could not find FA/SC .exe ${BASE_URI}/bin/SupremeCommander.exe`,
             'error'
           );
-          // try SupCom
         }
-
         exec(
           `"${BASE_URI}/bin/SupremeCommander.exe" /log "${BASE_URI_WIN}\\LOUD\\bin\\Loud.log" /init "${BASE_URI_WIN}\\LOUD\\bin\\LoudDataPath.lua"`
         );
